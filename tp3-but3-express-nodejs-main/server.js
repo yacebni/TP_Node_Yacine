c3df3dfaa1ca84ac5b6b7bfeeb6d6b91213a5082
Yacine <ya.bouanani@icloud.com>
2025-06-11 13:58:55 +0200

    Fix mongodb url security issue

--- tp3-but3-express-nodejs-main/server.js
+++ tp3-but3-express-nodejs-main/server.js
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 import { app } from './app.js'
-import dotenv from 'dotenv';
-import util from 'util';
-import { Tour } from './models/tour.model.js'
 //configure l'environnement
 //dotenv.config();
 //console.log(process.env.DATABASE)
 //const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD)
 
-const LOCAL_DATABASE = "mongodb+srv://yacinebouanani04:Fopr23sRcNrD2otc@cluster0.juxabuc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//const LOCAL_DATABASE = url here;
 async function connectDB() {
     await mongoose.connect(LOCAL_DATABASE)
 }
